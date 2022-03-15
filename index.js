f926ed47b3fad045b8b12426d56cb5dd7086a8d2
rusty619 <rusty6916@gmail.com>
2022-03-15 15:45:19 -0400

    added quiz function and button to use quiz

--- index.js
+++ index.js
@@ -1,5 +1,2 @@
 console.log('JavaScript is running')
 let score = 100;
-
-alert(score2)
-alert(score)
--- module.js
+++ module.js
@@ -1,16 +1,21 @@
-// let myName = prompt('Enter your name')
-// let month = prompt('Enter the month you were born')
-// let food = prompt('Enter what is your favorite food')
+
 /* 
  Functions
 
  1. Functions allows us to bundle together a lot of functionally and then easily call it all at once by name (function name)
 
- 2. Functions allow to specify 
+ 2. Functions allow to specify exactly WHEN we want to call all of this functionality
 
 */
 
 
-console.log("My Name is", myName)
-console.log("I Was born in", month)
-console.log("My favorite is", food)
+function quiz(){
+    let myName = prompt('Enter your name')
+    let month = prompt('Enter the month you were born')
+    let food = prompt('Enter what is your favorite food')
+
+    console.log("My Name is", myName)
+    console.log("I Was born in", month)
+    console.log("My favorite is", food)
+}
+
