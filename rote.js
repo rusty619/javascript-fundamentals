2e39eebd8e427068008e7c7d1a39f03105e90734
rusty619 <rusty6916@gmail.com>
2022-03-16 13:31:21 -0400

    Added functions on rote.js

---
+++ rote.js
@@ -0,0 +1,17 @@
+const greet = () => {
+    console.log("Hello World")
+}
+
+greet()
+
+const sqaure = (num) => {
+    return num * num
+}
+
+sqaure(25)
+
+const greetByName = (str) => {
+    console.log("Hello", str)
+} 
+
+greetByName('Ruslan')
