e8b93e13599e0acc46224af558a2b58f734ff9b6
rusty619 <rusty6916@gmail.com>
2022-03-16 12:10:02 -0400

    Function lessson on module.js

--- module.js
+++ module.js
@@ -9,7 +9,17 @@
 */
 
 
-function quiz(){
+// function quiz(){
+//     let myName = prompt('Enter your name')
+//     let month = prompt('Enter the month you were born')
+//     let food = prompt('Enter what is your favorite food')
+
+//     console.log("My Name is", myName)
+//     console.log("I Was born in", month)
+//     console.log("My favorite is", food)
+// }
+
+const quiz = () => {
     let myName = prompt('Enter your name')
     let month = prompt('Enter the month you were born')
     let food = prompt('Enter what is your favorite food')
@@ -17,5 +27,35 @@ function quiz(){
     console.log("My Name is", myName)
     console.log("I Was born in", month)
     console.log("My favorite is", food)
+}
+
+const print = () => {
+    console.log('I love to print!')
+}
+
+const functionA = (f) => {
+    console.log('FUNCTION A HAS BEEN CALLED!')
+    f()
+}
+
+const functionB = () => {
+    console.log('FUNCTION B HAS BEEN CALLED!')
+}
+
+const functionC = () => {
+    console.log("FUCTION C HAS BEEN CALLED!")
+}
+
+const greet = (person)=>{
+    console.log("HELLO!", person)
+}
+
+const blender = (string) => {
+    return string.split('')
+}
+
+const square = (num, message) => {
+    console.log('Message: ', message)
+    return num * num;
 }
 
