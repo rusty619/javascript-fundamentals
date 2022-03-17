b46fecc500340fee17dbe38557944dcebedd12ef
rusty619 <rusty6916@gmail.com>
2022-03-17 12:02:56 -0400

    index.js added building object with 3 apartment objects

---
+++ apartments/index.js
@@ -0,0 +1,49 @@
+// objects = {name: 'Ruslan', score: 100}
+// object2 = {street: 'Norman', zicode: 11222}
+
+// arrays = [100,200,300]
+// array2 = ['Ruslan', 'Dima', 'Paul']
+
+// user = {
+//     name: "Ruslan Khait",
+//     email: "ruslankhait@gmail.com",
+//     private: true,
+//     followers: 2,
+//     following: 100000
+// }
+
+// const followingCount = [100,200,300,400]
+
+let apt1 = {
+    bedrooms: 2,
+    sqft: 50,
+    bathrooms: 3,
+    windows: 0,
+    rent: 4500,
+    unit: '1A'
+}
+
+let apt2 = {
+    bedrooms: 1,
+    sqft: 25,
+    bathrooms: 1,
+    windows: 4,
+    rent: 3500,
+    unit: '1B'
+}
+
+let apt3 = {
+    bedrooms: 4,
+    sqft: 100,
+    bathrooms: 5,
+    windows: 10,
+    rent: 6000,
+    unit: '2A'
+}
+
+let building = {
+    streetAddress: '77 7th street, Brooklyn NY 11232',
+    laundry: false,
+    allowsPets: true,
+    apartments:[apt1,apt2,apt3]
+}
