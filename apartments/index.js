c593f4af26c62a62a2600d12784970fc472d383a
rusty619 <rusty6916@gmail.com>
2022-03-28 11:15:32 -0400

    Edited index.js with dom manulation

--- apartments/index.js
+++ apartments/index.js
@@ -99,3 +99,19 @@ let building = {
     }, 
     apartments:[apt1,apt2,apt3]
 }
+const div = document.getElementById('building')
+
+const render = () => {
+    let h2 = document.createElement('h2')
+    h2.innerText = building.streetAddress
+   // div.append(h2)
+    let ul = document.createElement('ul')
+    building.apartments.forEach((element) => {
+        let li = document.createElement('li')
+        li.innerText = element.unit
+        ul.append(li)
+    })
+    div.append(h2,ul)
+}
+
+render()
