a0550576c1f375a2d17a6a8086e691446b3cde70
rusty619 <rusty6916@gmail.com>
2022-03-28 11:34:42 -0400

    added button in index.js

--- apartments/index.js
+++ apartments/index.js
@@ -108,7 +108,18 @@ const render = () => {
     let ul = document.createElement('ul')
     building.apartments.forEach((element) => {
         let li = document.createElement('li')
-        li.innerText = element.unit
+        let button = document.createElement('button')
+        button.innerText = 'RENT ' + element.unit
+        button.innerText = `RENT: Vacancies: ${element.bedrooms}`
+        li.innerText = `UNIT ${element.unit}`
+
+        button.addEventListener('click', () => {
+            if(element.bedrooms <= 0 ) return alert("SORRY, NEW YORK IS DEAD")
+            let vacancies = element.bedrooms - 1
+            button.innerText = `RENT: Vacncies: ${vacancies}`
+            element.bedrooms -= 1
+        })
+        li.append(button)
         ul.append(li)
     })
     div.append(h2,ul)
